add6a09852c542b8b678e92bff4d8299978a6852
delgrange stanislas <stanislas.delgrange@sfr.fr>
2018-10-11 16:00:14 +0200

    test
    test

---
+++ shop/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer-area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                <p class="footer-text m-0" style="text-align">Copyright &copy;<script>
+                        document.write(new Date().getFullYear());
+
+                    </script>
+                    Tout droit reservée by FRANCK GOEFFREY STANISLAS</p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ shop/menu.php
@@ -0,0 +1,49 @@
+<header class="default-header">
+    <div class="menutop-wrap">
+        <div class="menu-top container">
+            <div class="d-flex justify-content-between align-items-center">
+                <ul class="list">
+                    <li><a href="tel:+12312-3-1209">03 04 05 06 07 </a></li>
+                    <li><a href="mailto:photomobile@photo.fr">photomobile@photo.fr</a></li>
+                </ul>
+                <ul class="list">
+                    <li><a href="login.php">Connection</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg  navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.png" href="index.php">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li><a href="index.php">Acceuil</a></li>
+                    <li><a href="category.php">shop</a></li>
+                    <li> <a href="cart.php">Panier</a></li>
+                    <!-- Dropdown -->
+                    <!--<li class="dropdown">
+                    <a class="dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        Pages
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="category.html">Category</a>
+                        <a class="dropdown-item" href="single.html">Single</a>
+                        <a class="dropdown-item" href="cart.html">Cart</a>
+                        <a class="dropdown-item" href="checkout.html">Checkout</a>
+                        <a class="dropdown-item" href="confermation.html">Confermation</a>
+                        <a class="dropdown-item" href="login.html">Login</a>
+                        <a class="dropdown-item" href="tracking.html">Tracking</a>
+                        <a class="dropdown-item" href="generic.html">Generic</a>
+                        <a class="dropdown-item" href="elements.html">Elements</a>
+                    </div>
+                </li>-->
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
